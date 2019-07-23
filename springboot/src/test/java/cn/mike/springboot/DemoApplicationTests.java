ba0db1a7856d8b37033e2c5fb49b0ff3a26e5b55
hisongsong1979 <53041745+hisongsong1979@users.noreply.github.com>
2019-07-23 11:37:32 +0800

    Initial commit

---
+++ springboot/src/test/java/cn/mike/springboot/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.mike.springboot;
+
+import org.unit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
